232742796fc81b8447923e05d89243bebc43028f
JuscelinoMamede <juscemamede01@gmail.com>
2024-02-29 14:24:24 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".home-title ", {
+  duration: 1000,
+});
+sr.reveal(".main-paragraph", {
+  origin: "top",
+  distance: "160px",
+  duration: 1000,
+  delay: 400,
+});
+sr.reveal(".h2-titles, .h3-titles", {
+  duration: 1000,
+  delay: 400,
+});
+sr.reveal(".gallery", {
+  origin: "left",
+  duration: 1500,
+  delay: 400,
+});
+sr.reveal(".israel-flag", {
+  origin: "bottom",
+  distance: "160px",
+  duration: 1000,
+  delay: 400,
+});
+sr.reveal(".img-p,.img-p2", {
+  origin: "left",
+  distance: "160px",
+  duration: 1000,
+  delay: 400,
+});
+sr.reveal("iframe", {
+  origin: "bottom",
+  distance: "160px",
+  duration: 1000,
+  delay: 400,
+});
+
+sr.reveal(".midias", { duration: 1000 });
